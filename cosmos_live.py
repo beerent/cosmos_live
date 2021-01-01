90b50ae8259a03fce7b6a45bf07565dcc6ae586a
beerent <brentryczak@gmail.com>
2021-01-01 14:42:41 -0600

    script now successfully uses auth key to query api driven by config data

--- cosmos_live.py
+++ cosmos_live.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from pathlib import Path
+import requests
+import time
 import json
 import sys
 
@@ -18,6 +20,9 @@ class ConfigReader:
 
 	def get_database_connection_info(self):
 		return self.config_json["database"][self.environment]
+
+	def get_api_info(self):
+		return self.config_json["api"][self.environment]
 
 	def get_json(self, key):
 		try:
@@ -64,34 +69,66 @@ class DatabaseConnector:
 class RestApiConnector:
 	admin_auth_key = None
 
-	def __init__(self, admin_auth_key):
+	def __init__(self, api_url, admin_auth_key):
+		self.api_url = api_url
 		self.admin_auth_key = admin_auth_key
 
+	def get_cosmos_live_session(self):
+		json = {"admin_auth_key" : self.admin_auth_key};
+		full_url = "%s/%s" % (self.api_url, "live")
+		response = requests.get(full_url, json)
 
-def get_database_connection_data(environment):
+		return response
+
+
+def get_database_connection_info(environment):
 	config_reader = ConfigReader(environment)
-	database_connection_data = config_reader.get_database_connection_info()
-	if database_connection_data == None:
+	database_connection_info = config_reader.get_database_connection_info()
+	if database_connection_info == None:
 		raise ValueError('database config data is invalid.')
 
-	return database_connection_data
+	return database_connection_info
 
-def get_database_connector(database_connection_data):
-	host = database_connection_data["host"]
-	database = database_connection_data["database"]
-	user = database_connection_data["user"]
-	password = database_connection_data["password"]
+def get_api_info(environment):
+	config_reader = ConfigReader(environment)
+	api_info = config_reader.get_api_info()
+	if api_info == None:
+		raise ValueError('api config data is invalid.')
+
+	return api_info
+
+def get_api_url(environment):
+	api_info = get_api_info(environment)
+	api_url = "%s:%s" % (api_info["host"], api_info["port"])
+
+	return api_url
+
+def get_database_connector(database_connection_info):
+	host = database_connection_info["host"]
+	database = database_connection_info["database"]
+	user = database_connection_info["user"]
+	password = database_connection_info["password"]
 
 	database_connector = DatabaseConnector(host, database, user, password)
 
 	return database_connector
 
+def run_cosmos_live(rest_api_connector):
+	while True:
+		session = rest_api_connector.get_cosmos_live_session()
+		print(session.json())
+		time.sleep(1)
+
 def main(environment):
-	database_connection_data = get_database_connection_data(environment)
-	database_connector = get_database_connector(database_connection_data)
+	database_connection_info = get_database_connection_info(environment)
+	database_connector = get_database_connector(database_connection_info)
 
 	admin_auth_key = database_connector.get_admin_auth_key()
-	rest_api_connector = RestApiConnector(admin_auth_key)
+	api_url = get_api_url(environment)
+
+	rest_api_connector = RestApiConnector(api_url, admin_auth_key)
+
+	run_cosmos_live(rest_api_connector)
 
 
 
@@ -104,4 +141,7 @@ if __name__ == "__main__":
 		print("error: must specify environment.")
 		exit(1)
 
-	main(sys.argv[1])
+	try:
+		main(sys.argv[1])
+	except KeyboardInterrupt:
+		sys.exit(0)
